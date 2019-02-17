8f884bf51d3984a5bf8458073e6c79cf7c2a4102
Dylan Maryk <dylan@door2door.io>
2019-02-17 12:32:30 +0100

    Add next question and that

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
     super(props);
     this.state = {
       showDialogue: false,
+      firstQuestion: true,
       items: [],
       income: "€ 1.000",
       relationships: "Doing well",
@@ -51,6 +52,13 @@ class App extends Component {
     setTimeout(() => {
       this.setState({
         relationships: relationships
+      });
+    }, this.timeout);
+  }
+  setFreetime(freetime) {
+    setTimeout(() => {
+      this.setState({
+        freetime: freetime
       });
     }, this.timeout);
   }
@@ -93,22 +101,45 @@ class App extends Component {
     }, this.timeout);
     this.timeout += 1000;
   }
+  updateQuestion(resetTimeout) {
+    setTimeout(() => {
+      this.setState({
+        firstQuestion: false
+      });
+      if (resetTimeout) {
+        this.timeout = 0;
+      }
+    }, this.timeout);
+  }
   addNextItems() {
     this.setState({
       items: ["img/feed-10.png"]
     });
     this.addItem("img/feed-11.png");
+    this.setStress(5);
+    this.setSleep("7.5h");
     this.addItem("img/feed-12.png");
     this.addItem("img/feed-13.png");
     this.addItem("img/feed-14.png");
+    this.updateQuestion();
+  }
+  addNextNextItems() {
+    this.addItem("img/feed-15.png");
+    this.addItem("img/feed-16.png");
+    this.addItem("img/feed-17.png");
+    this.addItem("img/feed-18.png");
+    this.addItem("img/feed-19.png");
+    this.addItem("img/feed-20.png");
+    this.setSleep("8.5h");
+    this.setFreetime("20h");
   }
   componentDidMount() {
     this.addItem("img/feed-0.png");
     this.addItem("img/space.png");
     this.addItem("img/feed-1.png");
     this.addItem("img/feed-2.png");
-    this.setIncome("€ 12.000")
-    this.setRelationships("Not great")
+    this.setIncome("€ 12.000");
+    this.setRelationships("Not great");
     this.addItem("img/space.png");
     this.addItem("img/space.png");
     this.addItem("img/feed-3.png");
@@ -117,8 +148,8 @@ class App extends Component {
     this.addItem("img/feed-5.png");
     this.addItem("img/space.png");
     this.addItem("img/feed-6.png");
-    this.setProfessional(2)
-    this.setSleep("6.5h")
+    this.setProfessional(2);
+    this.setSleep("6.5h");
     this.addItem("img/space.png");
     this.addItem("img/space.png");
     this.addItem("img/space.png");
@@ -130,14 +161,14 @@ class App extends Component {
     this.addItem("img/space.png");
     this.addItem("img/space.png");
     this.addItem("img/feed-9.png");
-    this.setStress(7)
-    this.setEmails(137)
+    this.setStress(7);
+    this.setEmails(137);
     this.addItem("img/space.png");
     this.addItem("img/space.png");
     this.addItem("img/space.png");
     this.addItem("img/space.png");
     this.addItem("img/feed-10.png");
-    this.setRelationships("Poor")
+    this.setRelationships("Poor");
     this.addItem("img/space.png");
     this.showButtons(true);
   }
@@ -152,20 +183,27 @@ class App extends Component {
               {
                 this.state.showDialogue ?
                 <Dialogue>
-                  <Text>
-                    Seems like your relationships are suffering a bit, maybe its time to take some less shifts at the hospital?
-                  </Text>
+                  {
+                    this.state.firstQuestion ?
+                    <Text>
+                      Seems like your relationships are suffering a bit, maybe its time to take some less shifts at the hospital?
+                    </Text>
+                    :
+                    <Text>
+                      You seem to be ready to make the jump to start your own practice
+                    </Text>
+                  }
                   <Ctas>
                     <button onClick={() => {}}>
                       No, thanks
                     </button>
-                    <button onClick={() => {this.addNextItems()}}>
+                    <button onClick={() => {this.state.firstQuestion ? this.addNextItems() : this.addNextNextItems()}}>
                       Yes
                     </button>
                   </Ctas>
                 </Dialogue>
                 :
-                  null
+                null
               }
               <IllustrationWrapper>
                 <img alt="" src="img/doctor-1.png" />
